b1ac2218f5d53de00a11a7795193b08e40c17f68
OmkarTerbhai <omkarterbhai@gmail.com>
2023-07-02 23:03:30 +0530

    Added datatypes demo.

---
+++ com/basics/DataTypes.java
@@ -0,0 +1,59 @@
+package com.basics;
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+        /*
+         * Boolean datatype:
+         * Can store only one of two values 'true' or 'false'
+         * Size 1 bit
+         */
+        boolean res = false;
+        System.out.println("Boolean: " + res);
+
+        /*
+         * Byte datatype:
+         * Can store integer values
+         * Range:- -128 to 127
+         * Size:- 1 byte
+         */
+        byte b = 120;
+        System.out.println("Byte: " + b);
+
+        /*
+         * Short datatype:
+         * Can store more integer values
+         * Range:- -32768 to 32767
+         * Size:- 2 bytes
+         */
+        short s = 1240;
+        System.out.println("Short: " + s);
+
+        /*
+         * Int datatype:
+         * Can store all integer values
+         * Range:- -2^31 to 2^31 - 1
+         * Size:- 4 bytes
+         */
+        int val = 234;
+        System.out.println("Int: " + val);
+
+        /*
+         * Char datatype:
+         * Can store single characters
+         * Range:- 0 to '\uffff'
+         * Size:- 2 bytes
+         */
+        char ch = 'A';
+        System.out.println("Char: " + ch);
+
+        /*
+         * Float datatype:
+         * Can store floating point(decimal numbers)
+         * Size: 4 bytes
+         */
+        float f = 32.89F;
+        System.out.println("Float: " + f);
+
+    }
+}
